8a9f67438580666f8402549b0f9832d824c8eff0
Jay Faulkner <jayleefaulkner@gmail.com>
2020-12-30 10:07:16 +0000

    refactor: use `_` convention for private member types

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -12,80 +12,77 @@ public class Ship : MonoBehaviour {
     [SerializeField] private float maxThrust = 100;
     [SerializeField] private float boostThrustMultiplier = 2;
     
-    private FlyDangerousActions shipActions;
-    private bool isBoosting = false;
+    private FlyDangerousActions _shipActions;
+    private bool _isBoosting = false;
     
     // input axes -1 to 1
-    private float throttle = 0;
-    private float pitch = 0;
-    private float yaw = 0;
-    private float roll = 0;
-    private float latV = 0;
-    private float latH = 0;
+    private float _throttle = 0;
+    private float _pitch = 0;
+    private float _yaw = 0;
+    private float _roll = 0;
+    private float _latV = 0;
+    private float _latH = 0;
 
-    private Transform transformComponent;
-    private Rigidbody rigidBodyComponent;
+    private Transform _transformComponent;
+    private Rigidbody _rigidBodyComponent;
     
     // Start is called before the first frame update
     private void Awake() {
-        transformComponent = GetComponent<Transform>();
-        rigidBodyComponent = GetComponent<Rigidbody>();
+        _transformComponent = GetComponent<Transform>();
+        _rigidBodyComponent = GetComponent<Rigidbody>();
 
         // TODO: revisit the canonical method of doing this junk with the preview package (sigh)
-        shipActions = new FlyDangerousActions();
-        shipActions.Ship.Pitch.performed += SetPitch;
-        shipActions.Ship.Pitch.canceled += SetPitch;
-        shipActions.Ship.Roll.performed += SetRoll;
-        shipActions.Ship.Roll.canceled += SetRoll;
-        shipActions.Ship.Yaw.performed += SetYaw;
-        shipActions.Ship.Yaw.canceled += SetYaw;
-        shipActions.Ship.Throttle.performed += SetThrottle;
-        shipActions.Ship.Throttle.canceled += SetThrottle;
-        shipActions.Ship.LateralH.performed += SetLateralH;
-        shipActions.Ship.LateralH.canceled += SetLateralH;
-        shipActions.Ship.LateralV.performed += SetLateralV;
-        shipActions.Ship.LateralV.canceled += SetLateralV;
-        shipActions.Ship.Boost.performed += Boost;
-        shipActions.Ship.Boost.canceled += Boost;
+        _shipActions = new FlyDangerousActions();
+        _shipActions.Ship.Pitch.performed += SetPitch;
+        _shipActions.Ship.Pitch.canceled += SetPitch;
+        _shipActions.Ship.Roll.performed += SetRoll;
+        _shipActions.Ship.Roll.canceled += SetRoll;
+        _shipActions.Ship.Yaw.performed += SetYaw;
+        _shipActions.Ship.Yaw.canceled += SetYaw;
+        _shipActions.Ship.Throttle.performed += SetThrottle;
+        _shipActions.Ship.Throttle.canceled += SetThrottle;
+        _shipActions.Ship.LateralH.performed += SetLateralH;
+        _shipActions.Ship.LateralH.canceled += SetLateralH;
+        _shipActions.Ship.LateralV.performed += SetLateralV;
+        _shipActions.Ship.LateralV.canceled += SetLateralV;
+        _shipActions.Ship.Boost.performed += Boost;
+        _shipActions.Ship.Boost.canceled += Boost;
     }
 
     private void OnEnable() {
-        shipActions.Enable();
+        _shipActions.Enable();
     }
 
     private void OnDisable() {
-        shipActions.Disable();
+        _shipActions.Disable();
     }
 
     public void SetPitch(InputAction.CallbackContext context) {
-        pitch = context.ReadValue<float>();
+        _pitch = context.ReadValue<float>();
     }
 
     public void SetRoll(InputAction.CallbackContext context) {
-        roll = context.ReadValue<float>();
+        _roll = context.ReadValue<float>();
     }
 
     public void SetYaw(InputAction.CallbackContext context) {
-        yaw = context.ReadValue<float>();
+        _yaw = context.ReadValue<float>();
     }
 
     public void SetThrottle(InputAction.CallbackContext context) {
-        throttle = context.ReadValue<float>();
+        _throttle = context.ReadValue<float>();
     }
     
     public void SetLateralH(InputAction.CallbackContext context) {
-        latH = context.ReadValue<float>();
+        _latH = context.ReadValue<float>();
     }
     
     public void SetLateralV(InputAction.CallbackContext context) {
-        latV = context.ReadValue<float>();
+        _latV = context.ReadValue<float>();
     }
 
     public void Boost(InputAction.CallbackContext context) {
-        isBoosting = context.ReadValueAsButton();
-        if (isBoosting) {
-            Debug.Log("ITS WORKING");
-        }
+        _isBoosting = context.ReadValueAsButton();
     }
 
     // Update is called once per frame - poll for input and game activity here (READ)
@@ -99,26 +96,26 @@ public class Ship : MonoBehaviour {
 
         // TODO: max thrust available to the system must be evenly split between the axes ?
         // otherwise we'll have the old goldeneye problem of travelling diagonally being the optimal play :|
-        float thrustMultiplier = isBoosting ? maxThrust * boostThrustMultiplier : maxThrust;
-        float torqueMultiplier = isBoosting ? maxThrust / 5 : maxThrust / 10;
+        float thrustMultiplier = _isBoosting ? maxThrust * boostThrustMultiplier : maxThrust;
+        float torqueMultiplier = _isBoosting ? maxThrust / 5 : maxThrust / 10;
         
-        if (throttle != 0) {
-            rigidBodyComponent.AddForce(transformComponent.forward * (throttle * thrustMultiplier), ForceMode.Force);
+        if (_throttle != 0) {
+            _rigidBodyComponent.AddForce(_transformComponent.forward * (_throttle * thrustMultiplier), ForceMode.Force);
         }
-        if (latH != 0) {
-            rigidBodyComponent.AddForce(transformComponent.right * (latH * thrustMultiplier), ForceMode.Force);
+        if (_latH != 0) {
+            _rigidBodyComponent.AddForce(_transformComponent.right * (_latH * thrustMultiplier), ForceMode.Force);
         }
-        if (latV != 0) {
-            rigidBodyComponent.AddForce(transformComponent.up * (latV * thrustMultiplier), ForceMode.Force);
+        if (_latV != 0) {
+            _rigidBodyComponent.AddForce(_transformComponent.up * (_latV * thrustMultiplier), ForceMode.Force);
         }
-        if (pitch != 0) {
-            rigidBodyComponent.AddTorque(transformComponent.right * (pitch * torqueMultiplier / 10), ForceMode.Force);
+        if (_pitch != 0) {
+            _rigidBodyComponent.AddTorque(_transformComponent.right * (_pitch * torqueMultiplier / 10), ForceMode.Force);
         }
-        if (yaw != 0) {
-            rigidBodyComponent.AddTorque(transformComponent.up * (yaw * torqueMultiplier / 10), ForceMode.Force);
+        if (_yaw != 0) {
+            _rigidBodyComponent.AddTorque(_transformComponent.up * (_yaw * torqueMultiplier / 10), ForceMode.Force);
         }
-        if (roll != 0) {
-            rigidBodyComponent.AddTorque(transformComponent.forward * (roll * torqueMultiplier / 10 * -1), ForceMode.Force);
+        if (_roll != 0) {
+            _rigidBodyComponent.AddTorque(_transformComponent.forward * (_roll * torqueMultiplier / 10 * -1), ForceMode.Force);
         }
     }
 }
