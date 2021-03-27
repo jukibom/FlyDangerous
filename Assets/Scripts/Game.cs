54fda4ab56b451b6f5c0c598363baa13515e88f3
Jay Faulkner <jayleefaulkner@gmail.com>
2021-03-27 17:42:56 +0000

    feat: pause game time scale when menu shown

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -10,6 +10,11 @@ public class Game : MonoBehaviour
     private FlyDangerousActions _gameActions;
     private Canvas _menuCanvas;
 
+    public bool isGameMenuActive {
+        get {
+            return this._menuCanvas.enabled;
+        }
+    }
     private void Awake() {
         this._menuCanvas = GetComponent<Canvas>();
         
@@ -21,16 +26,28 @@ public class Game : MonoBehaviour
     }
     
     private void OnEnable() {
-        _gameActions.Enable();
+        _gameActions.Global.Enable();
     }
 
     private void OnDisable() {
-        _gameActions.Disable();
+        _gameActions.Global.Disable();
     }
 
     public void ToggleMenu(InputAction.CallbackContext context) {
         if (context.ReadValueAsButton()) {
             this._menuCanvas.enabled = !this._menuCanvas.enabled;
+            this.handlePauseGameState();
+        }
+    }
+
+    // toggle ship controller input and timescales
+    private void handlePauseGameState() {
+        if (this.isGameMenuActive) {
+            // TODO: disable ship controls (must be same instance...)
+            Time.timeScale = 0;
+        }
+        else {
+            Time.timeScale = 1;
         }
     }
 }
