98f47893649bb24e0d71636ac31ca7e119f1d3de
Jay Faulkner <jayleefaulkner@gmail.com>
2021-04-03 19:00:10 +0100

    refactor: rethink how pause menu state works + improve animation

--- Assets/Scripts/Menus/PauseMenu.cs
+++ Assets/Scripts/Menus/PauseMenu.cs
@@ -5,75 +5,114 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
-[RequireComponent(typeof(Canvas))]
-public class PauseMenu : MonoBehaviour {
-
-    [Tooltip("Optional panel to animate on toggle")] [SerializeField]
-    private GameObject _mainPanel;
-    private FlyDangerousActions _gameActions;
-    private Canvas _menuCanvas;
-
-    public bool isGameMenuActive {
-        get {
-            return this._menuCanvas.enabled;
-        }
-    }
-    private void Awake() {
-        this._menuCanvas = GetComponent<Canvas>();
-        
-        this._gameActions = GlobalGameState.Actions;
-
-        this._gameActions.Global.GameMenuToggle.performed += ToggleMenuAction;
-        this._gameActions.Global.GameMenuToggle.canceled += ToggleMenuAction;
-        
-        this.HandlePauseGameState();
+namespace Menus {
+    public enum PauseMenuState {
+        Unpaused,
+        PausedMainMenu,
+        PausedOptionsMenu,
     }
     
-    private void OnEnable() {
-        _gameActions.Global.Enable();
-    }
+    [RequireComponent(typeof(Canvas))]
+    public class PauseMenu : MonoBehaviour {
 
-    private void OnDisable() {
-        _gameActions.Global.Disable();
-    }
+        [Tooltip("Used to animate the main panel")] [SerializeField]
+        private GameObject pauseMenuCanvas;
 
-    public void ToggleMenu() {
-        this._menuCanvas.enabled = !this._menuCanvas.enabled;
-        this.HandlePauseGameState();
-    }
+        [SerializeField]
+        private GameObject mainPanel;
 
-    public void Restart() {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    }
+        [SerializeField]
+        private GameObject optionsPanel;
 
-    public void Quit() {
-        Application.Quit();
-    }
-    
-    private void ToggleMenuAction(InputAction.CallbackContext context) {
-        if (context.ReadValueAsButton()) {
-            this._menuCanvas.enabled = !this._menuCanvas.enabled;
-            this.HandlePauseGameState();
-        }
-    }
-
-    // toggle ship controller input and timescales
-    private void HandlePauseGameState() {
-        if (this.isGameMenuActive) {
-            this._gameActions.Ship.Disable();
-            Time.timeScale = 0;
-        }
-        else {
-            this._gameActions.Ship.Enable();
-            Time.timeScale = 1;
-        }
-
-        if (_mainPanel != null) {
-            Animator animator = _mainPanel.GetComponent<Animator>();
-            if (animator != null) {
-                animator.SetBool("Open", this.isGameMenuActive);
+        private PauseMenuState _menuState = PauseMenuState.Unpaused;
+        public PauseMenuState MenuState {
+            get => this._menuState;
+            private set {
+                _menuState = value;
+                this.UpdatePauseGameState();
             }
         }
-        
+        private FlyDangerousActions _gameActions;
+        private Canvas _menuCanvas;
+        private Animator _panelAnimator;
+
+        private void Awake() {
+            this._menuCanvas = GetComponent<Canvas>();
+            this._panelAnimator = this.pauseMenuCanvas.GetComponent<Animator>();
+
+
+            this._gameActions = GlobalGameState.Actions;
+            this._gameActions.Global.Enable();
+
+            this._gameActions.Global.GameMenuToggle.performed += ToggleMenuAction;
+            this._gameActions.Global.GameMenuToggle.canceled += ToggleMenuAction;
+
+            this.UpdatePauseGameState();
+        }
+
+        public void Pause() {
+            this.MenuState = PauseMenuState.PausedMainMenu;
+            this._panelAnimator.SetBool("Open", true);
+        }
+
+        public void Resume() {
+            this.MenuState = PauseMenuState.Unpaused;
+            this._panelAnimator.SetBool("Open", false);
+            this._panelAnimator.Play("Standby");
+        }
+
+        public void Restart() {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+
+        public void OpenOptionsPanel() {
+            this.MenuState = PauseMenuState.PausedOptionsMenu;
+        }
+
+        public void CloseOptionsPanel() {
+            this.MenuState = PauseMenuState.PausedMainMenu;
+        }
+
+        public void Quit() {
+            Application.Quit();
+        }
+
+        private void ToggleMenuAction(InputAction.CallbackContext context) {
+            if (context.ReadValueAsButton()) {
+                switch (this.MenuState) {
+                    case PauseMenuState.Unpaused:
+                        Pause();
+                        break;
+                    case PauseMenuState.PausedMainMenu: 
+                        Resume();
+                        break;
+                    case PauseMenuState.PausedOptionsMenu:
+                        CloseOptionsPanel();
+                        break;
+                }
+            }
+        }
+
+        // toggle ship controller input and timescales
+        private void UpdatePauseGameState() {
+            switch (this.MenuState) {
+                case PauseMenuState.Unpaused:
+                    this._menuCanvas.enabled = false;
+                    this._gameActions.Ship.Enable();
+                    Time.timeScale = 1;
+                    break;
+                case PauseMenuState.PausedMainMenu: 
+                    this._menuCanvas.enabled = true;
+                    this._gameActions.Ship.Disable();
+                    this.optionsPanel.SetActive(false);
+                    this.mainPanel.SetActive(true);
+                    Time.timeScale = 0;
+                    break;
+                case PauseMenuState.PausedOptionsMenu:
+                    this.optionsPanel.SetActive(true);
+                    this.mainPanel.SetActive(false);
+                    break;
+            }
+        }
     }
 }
