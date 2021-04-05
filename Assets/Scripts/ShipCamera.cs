97d5203b20f99044e380fdd8c8ac73f8b32baf18
Jay Faulkner <jayleefaulkner@gmail.com>
2021-04-05 20:50:33 +0100

    feat: hugely improve that bullshit camera

--- Assets/Scripts/ShipCamera.cs
+++ Assets/Scripts/ShipCamera.cs
@@ -4,19 +4,28 @@ using UnityEngine;
 
 public class ShipCamera : MonoBehaviour {
 
-    public Transform target;
+    public Rigidbody target;
     public float smoothSpeed = 0.5f;
     public Vector3 offset;
 
     private Vector3 _velocity = Vector3.zero;
+
+    private Vector3 m_LastVelocity;
     
     // Update is called once per frame
     void FixedUpdate() {
         Quaternion targetRotation = target.rotation;
         Transform thisTransform = transform;
-        
-        Vector3 desiredPosition = target.position + (targetRotation * offset);
-        transform.position = Vector3.SmoothDamp(thisTransform.position, desiredPosition, ref _velocity, smoothSpeed);
         thisTransform.rotation = targetRotation;
+
+        if (m_LastVelocity != null) {
+            var acceleration = (target.velocity - m_LastVelocity) / Time.fixedDeltaTime;
+            var accelerationDelta = acceleration / 50;
+            
+            Vector3 desiredPosition = target.position + (targetRotation * offset) - accelerationDelta;
+            thisTransform.position = Vector3.SmoothDamp(thisTransform.position, desiredPosition, ref _velocity, smoothSpeed);
+        }
+
+        m_LastVelocity = target.velocity;
     }
 }
