ea888de33d49b850c64cee555cebe6637a500552
Jay Faulkner <jayleefaulkner@gmail.com>
2021-04-11 13:51:45 +0100

    feat: allow setting a distance on the floating origin + camera fix
    This allows the recalculation of the world to occur every x metres from origin - turns out recalculating the entire freaking world is rather expensive. Who knew?
    
    Also fixed the camera to stop trying to do a bunch of mental math to stay in place and just ... update the local transform and move the script to the child camera object where it belongs. I AM SMART.

--- Assets/Scripts/Engine/FloatingOrigin.cs
+++ Assets/Scripts/Engine/FloatingOrigin.cs
@@ -5,9 +5,19 @@ using UnityEngine;
 
 public class FloatingOrigin : MonoBehaviour {
     
+    [SerializeField]
     public GameObject focalObject;  // this should be the client player ship
+    
+    // Distance required to perform a correction. If 0, will occur every frame.
+    [SerializeField]
+    public float correctionDistance = 0.0f;
+    
     private Transform _worldTransform;
     private Transform _focalTransform;
+
+    public Vector3 focalObjectPosition {
+        get { return -this._worldTransform.position;  }
+    }
     
     // Start is called before the first frame update
     void Start() {
@@ -17,7 +27,9 @@ public class FloatingOrigin : MonoBehaviour {
     }
 
     void Update() {
-        this._worldTransform.position -= _focalTransform.position;
-        this._focalTransform.position = Vector3.zero;
+        if (_focalTransform.position.magnitude > correctionDistance) {
+            this._worldTransform.position -= _focalTransform.position;
+            this._focalTransform.position = Vector3.zero;
+        }
     }
 }
--- Assets/Scripts/ShipCamera.cs
+++ Assets/Scripts/ShipCamera.cs
@@ -13,13 +13,8 @@ public class ShipCamera : MonoBehaviour {
 
     private Vector3 _velocity = Vector3.zero;
     private Vector3 _lastVelocity;
-    private Transform _transform;
-    void Start() {
-        this._transform = this.transform;
-    }
 
     void Update() {
-
         var angularVelocity = target.angularVelocity;
         var angularMomentumModifier = target.rotation *
             Quaternion.Euler(
@@ -29,12 +24,11 @@ public class ShipCamera : MonoBehaviour {
             ); 
         
         Vector3 rotationModifier = angularMomentumModifier * offset;
-
+        
         var acceleration = (target.velocity - _lastVelocity) / Time.fixedDeltaTime;
         var accelerationDelta = acceleration / accelerationDampener / 100f;
         Vector3 desiredPosition = rotationModifier - accelerationDelta;
-        this._transform.position = Vector3.SmoothDamp(this._transform.position, desiredPosition, ref _velocity, smoothSpeed);
-        
+        this.transform.localPosition = Vector3.SmoothDamp(this.transform.localPosition, desiredPosition, ref _velocity, smoothSpeed);
         _lastVelocity = target.velocity;
     }
 }
