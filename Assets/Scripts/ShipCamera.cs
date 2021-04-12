e914c14d389f16ab4ee5b06d071e5508f43e6f77
Jay Faulkner <jayleefaulkner@gmail.com>
2021-04-12 13:20:59 +0100

    fix: more corrections to camera script
    sigh

--- Assets/Scripts/ShipCamera.cs
+++ Assets/Scripts/ShipCamera.cs
@@ -7,22 +7,22 @@ public class ShipCamera : MonoBehaviour {
 
     public Rigidbody target;
     public float smoothSpeed = 0.5f;
-    public Vector3 offset;
     public float accelerationDampener = 5f;
     public float angularMomentumDampener = 5f;
 
     private Vector3 _velocity = Vector3.zero;
     private Vector3 _lastVelocity;
 
-    void Update() {
-        var angularVelocity = target.angularVelocity;
-
-        Vector3 rotationModifier = (angularMomentumDampener * angularVelocity) + offset;
-        Vector3 rotated = Quaternion.AngleAxis(90, Vector3.forward) * rotationModifier;
+    void FixedUpdate() {
         
-        var acceleration = (target.velocity - _lastVelocity) / Time.fixedDeltaTime;
-        var accelerationDelta = acceleration / accelerationDampener / 100f;
-        Vector3 desiredPosition = rotated - accelerationDelta;
+        var angularVelocity = target.angularVelocity;
+        Vector3 rotationCameraModifier = Quaternion.AngleAxis(90, Vector3.back) * (angularMomentumDampener * angularVelocity);
+
+        var acceleration = transform.InverseTransformDirection(target.velocity - _lastVelocity) / Time.fixedDeltaTime;
+        var accelerationCameraDelta = -acceleration / accelerationDampener / 100f;
+        
+        Vector3 desiredPosition = accelerationCameraDelta - rotationCameraModifier;
+
         this.transform.localPosition = Vector3.SmoothDamp(this.transform.localPosition, desiredPosition, ref _velocity, smoothSpeed);
         _lastVelocity = target.velocity;
     }
