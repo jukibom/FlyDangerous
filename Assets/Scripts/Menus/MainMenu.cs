189093df0480c3d110de9738b8d3ab126594e864
Jay Faulkner <jayleefaulkner@gmail.com>
2021-05-01 23:42:38 +0100

    fix: prevent overblown emissive thruster from cutting through alpha msg
    whoops.

--- Assets/Scripts/Menus/MainMenu.cs
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -15,27 +15,23 @@ namespace Menus {
         private OptionsMenu optionsMenu;
 
         [SerializeField] private Animator crossfade;
-        [SerializeField] private Transform shipMeshTransform;
+        [SerializeField] private GameObject shipMesh;
         [SerializeField] private GameObject alphaMessage;
         
-        private Transform _transform;
-
         // Start is called before the first frame update
         void Awake() {
             StartCoroutine(ShowAlphaMessage());
-            _transform = transform;
         }
 
         // Update is called once per frame
         void FixedUpdate() {
             // move along at a fixed rate to animate the stars
             // dirty hack job but who cares it's a menu screen
-            _transform.Translate(0.1f, 0, 0.5f);
+            transform.Translate(0.1f, 0, 0.5f);
 
             // gently rock the ship mesh back and forth
             var rotationAmount = (0.25f - Mathf.PingPong(Time.time / 20, 0.5f)) / 5;
-            shipMeshTransform.Rotate(Vector3.forward, rotationAmount);
-
+            shipMesh.transform.Rotate(Vector3.forward, rotationAmount);
         }
 
         public void Race() {
@@ -73,11 +69,13 @@ namespace Menus {
             
             // if it's disabled in the editor don't show this fade animation
             if (alphaMessage.activeSelf) {
+                shipMesh.SetActive(false);
                 yield return new WaitForSeconds(6);
                 crossfade.SetTrigger("FadeToBlack");
                 yield return new WaitForSeconds(1);
                 alphaMessage.SetActive(false);
                 crossfade.SetTrigger("FadeFromBlack");
+                shipMesh.SetActive(true);
             }
         }
 
