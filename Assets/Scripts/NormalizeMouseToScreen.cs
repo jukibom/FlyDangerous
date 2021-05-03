f7be4780c3ea0804348d85290950aba4adb4b21b
Jay Faulkner <jayleefaulkner@gmail.com>
2021-05-03 20:41:13 +0100

    feat: custom input processor to normalize mouse screen values (-1 to 1)

---
+++ Assets/Scripts/NormalizeMouseToScreen.cs
@@ -0,0 +1,37 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+#if UNITY_EDITOR
+[InitializeOnLoad]
+#endif
+public class NormalizeMouseToScreen : InputProcessor<float>
+{
+    public enum ScreenDimension {
+        Width,
+        Height,
+    }
+
+    [Tooltip("Screen dimension to normalize by")]
+    public ScreenDimension dimension = ScreenDimension.Width;
+    
+    public override float Process(float value, InputControl control) {
+        var dimensionToNormalize = dimension == ScreenDimension.Width ? Screen.width : Screen.height;
+        
+        // Debug.Log("processor " + dimension + " (" + dimensionToNormalize + "), " + value + " => " + value / dimensionToNormalize);
+        return value / dimensionToNormalize;
+    }
+    
+#if UNITY_EDITOR
+    static NormalizeMouseToScreen()
+    {
+        Initialize();
+    }
+#endif
+
+    [RuntimeInitializeOnLoadMethod]
+    static void Initialize()
+    {
+        InputSystem.RegisterProcessor<NormalizeMouseToScreen>();
+    }
+}
