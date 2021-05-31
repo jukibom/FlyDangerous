ed6cdd5fda8ff81c41afa3289458e19dd8c57113
Jay Faulkner <jayleefaulkner@gmail.com>
2021-05-31 22:40:30 +0100

    feat: add resolution dropdown with populated options
    also chore: fix dropdowns to have working scroll

--- Assets/Scripts/Menus/Options/GraphicsPanel.cs
+++ Assets/Scripts/Menus/Options/GraphicsPanel.cs
@@ -1,13 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class GraphicsPanel : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
+public class GraphicsPanel : MonoBehaviour {
+
+    [SerializeField] private Dropdown resolutionDropdown;
+    [SerializeField] private Dropdown screenModeDropdown;
+
+    private Resolution[] _resolutions;
+
+    private void OnEnable() {
+        _resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+
+        var options = new List<string>();
+        var currentResolutionIndex = 0;
         
+        for (int i = 0; i < _resolutions.Length; i++) {
+            var resolution = _resolutions[i];
+            var option = resolution.width + " x " + resolution.height;
+            options.Add(option);
+
+            if (resolution.width == Screen.currentResolution.width &&
+                resolution.height == Screen.currentResolution.height) {
+                currentResolutionIndex = i;
+            }
+        }
+        
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
     }
 
     // Update is called once per frame
