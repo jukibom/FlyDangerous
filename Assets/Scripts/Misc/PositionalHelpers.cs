1983dc4a916fc61c43c11a30d363fb52ef227b1a
Jay Faulkner <jayleefaulkner@gmail.com>
2021-07-29 19:39:27 +0100

    feat: take terrain into account when dynamically positioning ships
    this means that ships cannot spawn underground and we now have a generic terrain height position function and the ability to get the closest terrain tile

--- Assets/Scripts/Misc/PositionalHelpers.cs
+++ Assets/Scripts/Misc/PositionalHelpers.cs
@@ -1,14 +1,63 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Linq;
+using Den.Tools;
+using JetBrains.Annotations;
+using MapMagic.Core;
+using UnityEngine;
 
 namespace Misc {
     public static class PositionalHelpers {
+        
+        [CanBeNull] public static Terrain GetClosestCurrentTerrain(Vector3 position) {
+            //Get all terrain
+            Terrain[] terrains = Terrain.activeTerrains;
+
+            //Make sure that terrains length is ok
+            if (terrains.Length == 0)
+                return null;
+
+            //If just one, return that one terrain
+            if (terrains.Length == 1)
+                return terrains[0];
+
+            //Get the closest one to the player
+            float lowDist = (terrains[0].GetPosition() - position).sqrMagnitude;
+            var terrainIndex = 0;
+
+            for (int i = 1; i < terrains.Length; i++)
+            {
+                Terrain terrain = terrains[i];
+                Vector3 terrainPos = terrain.GetPosition();
+
+                //Find the distance and check if it is lower than the last one then store it
+                var dist = (terrainPos - position).sqrMagnitude;
+                if (dist < lowDist)
+                {
+                    lowDist = dist;
+                    terrainIndex = i;
+                }
+            }
+            return terrains[terrainIndex];
+        }
+        
+        public static Vector3 ClampPositionToTerrain(Vector3 position, float padding) {
+            // if there's a terrain, clamp the height to that of the height map + padding at a minimum
+            var terrain = GetClosestCurrentTerrain(position);
+            if (terrain != null) {
+                position.y = Mathf.Max(
+                    position.y, 
+                    terrain.transform.position.y + terrain.SampleHeight(position) + padding
+                );
+            }
+            return position;
+        }
 
         // generate a valid location to spawn an object by checking in a Fibonacci sphere of ever-increasing radius until a position is found.
         public static Vector3 FindClosestEmptyPosition(Vector3 originalPosition, int objectRadius) {
             // -1 in this instance effectively means the original position with no transformation
             int n = -1;
-            int max = 20;
-            int radius = 50;
+            int max = 40;
+            int positionSphereRadius = 50;
             Collider[] hitColliders = new Collider[1];
             Vector3 testPosition = originalPosition;
 
@@ -18,8 +67,10 @@ namespace Misc {
             // set the next position and perform a simple sphere collision check, return the number of
             bool NextPositionIsObstructed() {
                 if (n != -1) {
-                    testPosition = originalPosition + FibSpherePosition(n, max, radius);   
+                    testPosition = originalPosition + FibSpherePosition(n, max, positionSphereRadius);   
                 }
+
+                testPosition = ClampPositionToTerrain(testPosition, objectRadius);
                 Physics.OverlapSphereNonAlloc(testPosition, objectRadius, hitColliders, collisionLayerMask);
                 return hitColliders[0] != null;
             }
@@ -30,14 +81,9 @@ namespace Misc {
                 n++;
                 if (n == max) {
                     n = 0;
-                    radius += 20;
+                    positionSphereRadius += 20;
                 }
-
-                Debug.Log("Failed to position ship at " + testPosition);
             }
-
-            Debug.Log("Positioning ship at " + testPosition);
-
             return testPosition;
         }
         
