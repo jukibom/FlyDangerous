c3771b58248de83afa2d5a785d557ca5e76e442a
Jay Faulkner <jayleefaulkner@gmail.com>
2022-01-02 19:39:24 +0000

    fix: increased ship speed on menu (less gross trails at start)

--- Assets/Scripts/Menus/Main Menu/MainMenu.cs
+++ Assets/Scripts/Menus/Main Menu/MainMenu.cs
@@ -24,6 +24,8 @@ namespace Menus.Main_Menu {
         [SerializeField] private TopMenu topMenu;
         [SerializeField] private DisconnectionDialog disconnectionDialog;
 
+        [SerializeField] private float shipSpeed = 6f;
+
         public static bool FirstRun => Game.Instance?.MenuFirstRun ?? true;
 
         private void Start() {
@@ -48,7 +50,7 @@ namespace Menus.Main_Menu {
         private void FixedUpdate() {
             // move along at a fixed rate to animate the stars
             // dirty hack job but who cares it's a menu screen
-            transform.Translate(0.1f, 0, 0.5f);
+            transform.Translate(0.1f, 0, shipSpeed);
 
             // gently rock the ship mesh back and forth
             var rotationAmount = (0.25f - Mathf.PingPong(Time.time / 20, 0.5f)) / 5;
