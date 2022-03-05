6f33eedc5507da785f1aac33bb8540cedecb9699
Jay Faulkner <jayleefaulkner@gmail.com>
2022-03-05 17:09:38 +0000

    feat: add always-on smoke with a camera alpha fade cutoff
    this way it doesn't clog up the screen while idle but looks good when no thrust applied in FAoff etc. Also shifted the positional jitter to a factor of local z speed rather than thrust

--- Assets/Scripts/Core/Ship/ThrustTrail.cs
+++ Assets/Scripts/Core/Ship/ThrustTrail.cs
@@ -29,8 +29,8 @@ namespace Core.Ship {
         public void UpdateThrustTrail(Vector3 vesselSpeed, float maxSpeed, Vector3 force) {
             var vesselSpeedLocal = _transform.InverseTransformDirection(vesselSpeed);
 
-            _trailEffect.SetVector3("_startingVelocityMin", vesselSpeedLocal + minEjectionSpeed * force.z);
-            _trailEffect.SetVector3("_startingVelocityMax", vesselSpeedLocal + maxEjectionSpeed * force.z);
+            _trailEffect.SetVector3("_startingVelocityMin", vesselSpeedLocal + minEjectionSpeed * Math.Max(0.1f, force.z));
+            _trailEffect.SetVector3("_startingVelocityMax", vesselSpeedLocal + maxEjectionSpeed * Math.Max(0.1f, force.z));
             
             // only show with forward thrust and set the spawn rate to the ratio of thrust over max 
             int spawnRate = force.z > 0.001f
@@ -43,10 +43,10 @@ namespace Core.Ship {
                         force.z
                     )
                 ) 
-                : 0;
+                : minSpawnRate;
 
             _trailEffect.SetInt("_spawnRate", spawnRate);
-            _trailEffect.SetVector3("_startingPositionJitter", new Vector3(0, 0, MathfExtensions.Remap(0, 1, 0, -5, force.z)));
+            _trailEffect.SetVector3("_startingPositionJitter", new Vector3(0, 0, MathfExtensions.Remap(0, 1, 0, -5, vesselSpeedLocal.z / maxSpeed)));
         }
     }
 }
