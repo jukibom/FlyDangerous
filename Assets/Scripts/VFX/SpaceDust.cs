a757097e5e2953ccebe6e0367b5d175f4cf8979e
Jay Faulkner <jayleefaulkner@gmail.com>
2022-03-08 17:22:01 +0000

    feat: replace space dust sprites with velocity-driven line particles

--- Assets/Scripts/VFX/SpaceDust.cs
+++ Assets/Scripts/VFX/SpaceDust.cs
@@ -2,22 +2,41 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Core;
+using Core.Player;
+using Misc;
 using UnityEngine;
 using UnityEngine.VFX;
 
 public class SpaceDust : MonoBehaviour {
     [SerializeField]
     private bool forceOn;
-    private VisualEffect _VFX;
+    private VisualEffect _vfx;
+    private ShipPlayer _player;
+    private Transform _playerShipTransform;
+    private Rigidbody _playerShipRigidbody;
 
-    private void Start() {
-        _VFX = GetComponent<VisualEffect>();
+    private void OnEnable() {
+        _vfx = GetComponent<VisualEffect>(); 
+        _player = FdPlayer.FindLocalShipPlayer;
+        if (_player) {
+            _playerShipTransform = _player.GetComponent<Transform>();
+            _playerShipRigidbody = _player.GetComponent<Rigidbody>();
+        }
     }
 
-    // Update is called once per frame
     void Update() {
         if (!forceOn) {
-            _VFX.enabled = Preferences.Instance.GetBool("showSpaceDust");
+            _vfx.enabled = Preferences.Instance.GetBool("showSpaceDust");
         }
+
+        if (_player) {
+            _vfx.SetVector3("_playerVelocity",
+                _playerShipTransform.InverseTransformDirection(_playerShipRigidbody.velocity));
+            _vfx.SetVector3("_playerVelocity", _playerShipRigidbody.velocity);
+            _vfx.SetFloat("_alphaMultiplier", MathfExtensions.Remap(0.1f, 1, 0, 0.4f, _player.VelocityNormalised));
+        }
+
+        // lock the transform in world space so we don't rotate with the ship
+        transform.rotation = Quaternion.identity;
     }
 }
