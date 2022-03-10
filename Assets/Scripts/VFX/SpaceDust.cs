887bbc3efbaea842dba291beeb4da5cb28b38fdf
Jay Faulkner <jayleefaulkner@gmail.com>
2022-03-10 21:51:28 +0000

    chore: shift space dust vfx into vfx namespace

--- Assets/Scripts/VFX/SpaceDust.cs
+++ Assets/Scripts/VFX/SpaceDust.cs
@@ -1,42 +1,41 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using Core;
 using Core.Player;
 using Misc;
 using UnityEngine;
 using UnityEngine.VFX;
 
-public class SpaceDust : MonoBehaviour {
-    [SerializeField]
-    private bool forceOn;
-    private VisualEffect _vfx;
-    private ShipPlayer _player;
-    private Transform _playerShipTransform;
-    private Rigidbody _playerShipRigidbody;
+namespace VFX {
+    public class SpaceDust : MonoBehaviour {
+        [SerializeField]
+        private bool forceOn;
+        private VisualEffect _vfx;
+        private ShipPlayer _player;
+        private Transform _playerShipTransform;
+        private Rigidbody _playerShipRigidbody;
 
-    private void OnEnable() {
-        _vfx = GetComponent<VisualEffect>(); 
-        _player = FdPlayer.FindLocalShipPlayer;
-        if (_player) {
-            _playerShipTransform = _player.GetComponent<Transform>();
-            _playerShipRigidbody = _player.GetComponent<Rigidbody>();
-        }
-    }
-
-    void Update() {
-        if (!forceOn) {
-            _vfx.enabled = Preferences.Instance.GetBool("showSpaceDust");
+        private void OnEnable() {
+            _vfx = GetComponent<VisualEffect>(); 
+            _player = FdPlayer.FindLocalShipPlayer;
+            if (_player) {
+                _playerShipTransform = _player.GetComponent<Transform>();
+                _playerShipRigidbody = _player.GetComponent<Rigidbody>();
+            }
         }
 
-        if (_player) {
-            _vfx.SetVector3("_playerVelocity",
-                _playerShipTransform.InverseTransformDirection(_playerShipRigidbody.velocity));
-            _vfx.SetVector3("_playerVelocity", _playerShipRigidbody.velocity);
-            _vfx.SetFloat("_alphaMultiplier", MathfExtensions.Remap(0.1f, 1, 0, 0.4f, _player.VelocityNormalised));
-        }
+        void Update() {
+            if (!forceOn) {
+                _vfx.enabled = Preferences.Instance.GetBool("showSpaceDust");
+            }
 
-        // lock the transform in world space so we don't rotate with the ship
-        transform.rotation = Quaternion.identity;
+            if (_player) {
+                _vfx.SetVector3("_playerVelocity",
+                    _playerShipTransform.InverseTransformDirection(_playerShipRigidbody.velocity));
+                _vfx.SetVector3("_playerVelocity", _playerShipRigidbody.velocity);
+                _vfx.SetFloat("_alphaMultiplier", MathfExtensions.Remap(0.1f, 1, 0, 0.4f, _player.VelocityNormalised));
+            }
+
+            // lock the transform in world space so we don't rotate with the ship
+            transform.rotation = Quaternion.identity; 
+        }
     }
 }
