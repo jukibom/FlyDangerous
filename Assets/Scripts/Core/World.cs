003071faf9eeddc3ed6c2948cf54f9c86bd65f7a
Jay Faulkner <jayleefaulkner@gmail.com>
2022-03-16 01:19:38 +0000

    feat: handle floating origin culling flicker for gpu instances

--- Assets/Scripts/Core/World.cs
+++ Assets/Scripts/Core/World.cs
@@ -1,3 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using GPUInstancer;
+using JetBrains.Annotations;
+using MapMagic.Core;
 using UnityEngine;
 
 namespace Core {
@@ -10,8 +15,72 @@ namespace Core {
             FloatingOrigin.OnFloatingOriginCorrection -= PerformCorrection;
         }
 
+#if !NO_PAID_ASSETS
+        private readonly List<GPUInstancerDetailManager> _detailManagers = new();
+        private GPUInstancerTreeManager _gpuInstancerTreeManager;
+
+        [CanBeNull]
+        private GPUInstancerTreeManager GPUInstancerTreeManager {
+            get {
+                if (_gpuInstancerTreeManager == null) _gpuInstancerTreeManager = FindObjectOfType<GPUInstancerTreeManager>();
+
+                return _gpuInstancerTreeManager;
+            }
+        }
+
+        private MapMagicObject _mapMagicObject;
+
+        [CanBeNull]
+        private MapMagicObject MapMagicObject {
+            get {
+                if (_mapMagicObject == null) _mapMagicObject = FindObjectOfType<MapMagicObject>();
+
+                return _mapMagicObject;
+            }
+        }
+#endif
+
         private void PerformCorrection(Vector3 offset) {
             foreach (Transform child in transform) child.position -= offset;
+
+#if NO_PAID_ASSETS
         }
+#else
+            StartCoroutine(HandleGPUCulling());
+        }
+
+        /**
+         * On floating point correction, GPU culling cannot work as it relies on the depth buffer.
+         * The depth buffer takes a frame to calculate so for this frame we're going to take the GPU
+         * performance hit and disable culling until the next render update.
+         */
+        private IEnumerator HandleGPUCulling() {
+            if (GPUInstancerTreeManager) {
+                GPUInstancerTreeManager.isFrustumCulling = false;
+                GPUInstancerTreeManager.isOcclusionCulling = false;
+            }
+
+            var mapMagicObject = MapMagicObject;
+            if (mapMagicObject != null) {
+                mapMagicObject.GetComponentsInChildren(false, _detailManagers);
+                foreach (var gpuInstancerDetailManager in _detailManagers) {
+                    gpuInstancerDetailManager.isFrustumCulling = false;
+                    gpuInstancerDetailManager.isOcclusionCulling = false;
+                }
+            }
+
+            yield return new WaitForEndOfFrame();
+
+            if (GPUInstancerTreeManager) {
+                GPUInstancerTreeManager.isFrustumCulling = true;
+                GPUInstancerTreeManager.isOcclusionCulling = true;
+            }
+
+            foreach (var gpuInstancerDetailManager in _detailManagers) {
+                gpuInstancerDetailManager.isFrustumCulling = true;
+                gpuInstancerDetailManager.isOcclusionCulling = true;
+            }
+        }
+#endif
     }
 }
