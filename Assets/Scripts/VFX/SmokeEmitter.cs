fdecc54b3d98f564319f2eb9908f0d6838b97ad5
Jay Faulkner <jayleefaulkner@gmail.com>
2022-03-26 18:18:32 +0000

    fix: failing to initialise other players' smoke trails on client connect

--- Assets/Scripts/VFX/SmokeEmitter.cs
+++ Assets/Scripts/VFX/SmokeEmitter.cs
@@ -12,12 +12,16 @@ namespace Core.Ship {
         [SerializeField] private int minSpawnRate = 3;
         [SerializeField] private int maxSpawnRate = 150;
 
+        // fix problem with async updating with multiplayer initialisation (client can fail to update trails and disconnect with null exception)
+        private bool _ready;
+
         private VisualEffect _trailEffect;
         private Transform _transform;
 
-        private void OnEnable() {
+        private void Awake() {
             _trailEffect = GetComponent<VisualEffect>();
             _transform = transform;
+            _ready = true;
         }
 
         public void UpdateColor(Color color) {
@@ -25,27 +29,29 @@ namespace Core.Ship {
         }
 
         public void UpdateThrustTrail(Vector3 vesselSpeed, float maxSpeed, Vector3 force) {
-            var vesselSpeedLocal = _transform.InverseTransformDirection(vesselSpeed);
+            if (_ready) {
+                var vesselSpeedLocal = _transform.InverseTransformDirection(vesselSpeed);
 
-            _trailEffect.SetVector3("_startingVelocityMin", vesselSpeedLocal + minEjectionSpeed * Math.Max(0.2f, force.z));
-            _trailEffect.SetVector3("_startingVelocityMax", vesselSpeedLocal + maxEjectionSpeed * Math.Max(0.2f, force.z));
+                _trailEffect.SetVector3("_startingVelocityMin", vesselSpeedLocal + minEjectionSpeed * Math.Max(0.2f, force.z));
+                _trailEffect.SetVector3("_startingVelocityMax", vesselSpeedLocal + maxEjectionSpeed * Math.Max(0.2f, force.z));
 
-            // only show with forward thrust and set the spawn rate to the ratio of thrust over max 
-            var spawnRate = force.z > 0.001f
-                ? MathfExtensions.Remap(
-                    0,
-                    1,
-                    minSpawnRate,
-                    maxSpawnRate,
-                    force.z
-                )
-                // set minimum spawn rate as a factor of velocity 
-                : MathfExtensions.Remap(0, maxSpeed, minSpawnRate, maxSpawnRate / 2f, vesselSpeedLocal.z);
+                // only show with forward thrust and set the spawn rate to the ratio of thrust over max 
+                var spawnRate = force.z > 0.001f
+                    ? MathfExtensions.Remap(
+                        0,
+                        1,
+                        minSpawnRate,
+                        maxSpawnRate,
+                        force.z
+                    )
+                    // set minimum spawn rate as a factor of velocity 
+                    : MathfExtensions.Remap(0, maxSpeed, minSpawnRate, maxSpawnRate / 2f, vesselSpeedLocal.z);
 
-            _trailEffect.SetInt("_spawnRate", Mathf.FloorToInt(spawnRate));
+                _trailEffect.SetInt("_spawnRate", Mathf.FloorToInt(spawnRate));
 
-            // set jitter to a factor of the forward local velocity 
-            _trailEffect.SetVector3("_startingPositionJitter", new Vector3(0, 0, MathfExtensions.Remap(0, 1, 0, -5, vesselSpeedLocal.z / maxSpeed)));
+                // set jitter to a factor of the forward local velocity 
+                _trailEffect.SetVector3("_startingPositionJitter", new Vector3(0, 0, MathfExtensions.Remap(0, 1, 0, -5, vesselSpeedLocal.z / maxSpeed)));
+            }
         }
     }
 }
