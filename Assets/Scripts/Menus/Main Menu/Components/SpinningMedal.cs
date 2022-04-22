bd9ab4137d3294be56b5c0bce83ecc8b61d6592a
Jay Faulkner <jayleefaulkner@gmail.com>
2022-04-22 17:28:23 +0100

    tweak: offset rotations of medal models

--- Assets/Scripts/Menus/Main Menu/Components/SpinningMedal.cs
+++ Assets/Scripts/Menus/Main Menu/Components/SpinningMedal.cs
@@ -1,23 +1,25 @@
-using System;
 using NaughtyAttributes;
 using UnityEngine;
 
 namespace Menus.Main_Menu.Components {
     public class SpinningMedal : MonoBehaviour {
         [SerializeField] private GameObject medalModel;
-        [Label("Rotation (Degrees / Second)")][SerializeField] private float rotationDegrees = 180;
+
+        [Label("Rotation (Degrees / Second)")] [SerializeField]
+        private float rotationDegrees = 180;
 
         [Label("Sync with transform on enable (optional)")] [SerializeField]
         private Transform syncWith;
-
-        private void OnEnable() {
-            if (syncWith != null) medalModel.transform.localRotation = syncWith.localRotation;
-        }
 
         private void Update() {
             var modelTransform = medalModel.transform;
             modelTransform.localPosition = Vector3.zero;
             modelTransform.RotateAround(modelTransform.position, Vector3.up, rotationDegrees * Time.deltaTime);
+        }
+
+        private void OnEnable() {
+            if (syncWith != null) medalModel.transform.localRotation = syncWith.localRotation;
+            medalModel.transform.RotateAround(medalModel.transform.position, Vector3.up, -20);
         }
     }
 }
