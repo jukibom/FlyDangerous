65e91af8a2aa81d550acbd2561f2e8d994d4d2bb
Jay Faulkner <jayleefaulkner@gmail.com>
2022-04-25 23:25:54 +0100

    fix: incorrect conversion to / from milliseconds on leaderboard
    bloody hell don't I feel daft writing that whatever-the-fuck-that-was

--- Assets/Scripts/Misc/TimeExtensions.cs
+++ Assets/Scripts/Misc/TimeExtensions.cs
@@ -1,21 +1,13 @@
 ﻿using System;
+using UnityEngine;
 
 namespace Misc {
     public class TimeExtensions {
         public static string TimeSecondsToString(float timeSeconds) {
-            var addMinus = timeSeconds < 0;
-            timeSeconds = Math.Abs(timeSeconds);
-
-            var hours = (int)(timeSeconds / 3600);
-            var minutes = (int)(timeSeconds / 60) % 60;
-            var seconds = (int)timeSeconds % 60;
-            var fraction = (int)(timeSeconds * 100) % 100;
-
-            var text = hours > 0
-                ? $"{hours:00}:{minutes:00}:{seconds:00}:{fraction:00}"
-                : $"{minutes:00}:{seconds:00}:{fraction:00}";
-
-            return addMinus ? $"-{text}" : text;
+            var t = TimeSpan.FromSeconds(Mathf.Abs(timeSeconds));
+            // return in 00:00:00 format (tens of milliseconds - we're only accurate to 1/50th of a second anyway!)
+            var time = $"{t.Minutes:D2}:{t.Seconds:D2}:{t.Milliseconds / 10:D2}";
+            return timeSeconds < 0 ? $"-{time}" : time;
         }
     }
 }
