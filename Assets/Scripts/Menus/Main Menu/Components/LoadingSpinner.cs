eeb01b68b5cf862b53ce7d3f31b4e58dfae34110
Jay Faulkner <jayleefaulkner@gmail.com>
2022-04-28 23:32:22 +0100

    chore: tweak loading spinner rotation speed

--- Assets/Scripts/Menus/Main Menu/Components/LoadingSpinner.cs
+++ Assets/Scripts/Menus/Main Menu/Components/LoadingSpinner.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 namespace Menus.Main_Menu.Components {
     public class LoadingSpinner : MonoBehaviour {
-        public Vector3 rotation = new(0, 0, -1);
+        public Vector3 rotation = new(0, 0, -5);
 
         private void FixedUpdate() {
             transform.Rotate(rotation);
