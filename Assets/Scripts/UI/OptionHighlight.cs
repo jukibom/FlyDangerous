5980a172b6029dd74830922ad360c965ec9effd1
Jay Faulkner <jayleefaulkner@gmail.com>
2022-04-28 23:30:37 +0100

    fix: improve option highlight event propagation to include click events

--- Assets/Scripts/UI/OptionHighlight.cs
+++ Assets/Scripts/UI/OptionHighlight.cs
@@ -1,7 +1,6 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Audio;
 using UI;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -9,42 +8,28 @@ using UnityEngine.UI;
 
 [RequireComponent(typeof(Image))]
 [RequireComponent(typeof(SelectableElement))]
-public class OptionHighlight : MonoBehaviour, ISelectHandler, IDeselectHandler, IPointerEnterHandler, IPointerExitHandler, ISubmitHandler {
+public class OptionHighlight : MonoBehaviour, ISelectHandler, IDeselectHandler, IPointerEnterHandler, IPointerExitHandler, ISubmitHandler,
+    IPointerClickHandler {
     [SerializeField] private Image selectionImage;
     [SerializeField] private Selectable interactionObject;
-    [SerializeField] private bool hightlightOnlyOnSubmit;
-
-    private bool _selected;
+    [SerializeField] private bool highlightOnlyOnSubmit;
     private Color _baseColor;
     private Color _hoverColor;
-    
+
     private List<Selectable> _selectables;
-    
+
+    private bool _selected;
+
     private void OnEnable() {
         selectionImage.enabled = false;
         _baseColor = selectionImage.color;
         _hoverColor = new Color(_baseColor.r, _baseColor.g, _baseColor.b, _baseColor.a / 2);
         GetComponent<SelectableElement>().playSound = false;
-        
+
         // get all selectable elements in the hierarchy excluding this one
         _selectables = GetComponentsInChildren<Selectable>()
             .ToList()
             .FindAll(s => s.gameObject != gameObject);
-    }
-
-    public void OnSelect(BaseEventData eventData) {
-        _selected = true;
-        selectionImage.enabled = true;
-        selectionImage.color = _baseColor;
-
-        IEnumerator SelectOption() {
-            yield return new WaitForEndOfFrame();
-            var anyChildSelected = _selectables.Find(s => EventSystem.current.currentSelectedGameObject == s.gameObject) != null;
-            if (_selected && !anyChildSelected)
-                interactionObject.Select();
-        }
-
-        if (!hightlightOnlyOnSubmit) StartCoroutine(SelectOption());
     }
 
     public void OnDeselect(BaseEventData eventData) {
@@ -52,18 +37,48 @@ public class OptionHighlight : MonoBehaviour, ISelectHandler, IDeselectHandler, 
         _selected = false;
     }
 
+    public void OnPointerClick(PointerEventData eventData) {
+        ActivateElement();
+    }
+
     public void OnPointerEnter(PointerEventData eventData) {
         selectionImage.enabled = true;
-        if (!_selected) selectionImage.color = _hoverColor;
+        if (!_selected) {
+            selectionImage.color = _hoverColor;
+            PlaySound();
+        }
     }
 
     public void OnPointerExit(PointerEventData eventData) {
-        if (!_selected) selectionImage.enabled = false;
+        if (!_selected) {
+            selectionImage.enabled = false;
+            _selected = false;
+        }
+    }
+
+    public void OnSelect(BaseEventData eventData) {
+        if (!_selected) PlaySound();
+        _selected = true;
+        selectionImage.enabled = true;
+        selectionImage.color = _baseColor;
     }
 
     public void OnSubmit(BaseEventData eventData) {
-        if (hightlightOnlyOnSubmit) {
+        ActivateElement();
+    }
+
+    private void ActivateElement() {
+        if (highlightOnlyOnSubmit) {
             interactionObject.Select();
         }
+        else {
+            var ped = new PointerEventData(EventSystem.current);
+            ExecuteEvents.Execute(interactionObject.gameObject, ped, ExecuteEvents.submitHandler);
+        }
+    }
+
+    private void PlaySound() {
+        var audioManager = UIAudioManager.Instance;
+        if (audioManager != null) audioManager.Play("ui-nav");
     }
 }
