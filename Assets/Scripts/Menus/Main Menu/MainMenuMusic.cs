967bda33d1a751e8166dc6d45d202992fe1164cf
Jay Faulkner <jayleefaulkner@gmail.com>
2022-04-30 12:39:34 +0100

    chore: restructure music to prepare for dynamic loading (resources dir)

--- Assets/Scripts/Menus/Main Menu/MainMenuMusic.cs
+++ Assets/Scripts/Menus/Main Menu/MainMenuMusic.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using UnityEngine;
 
@@ -6,15 +5,43 @@ namespace Menus.Main_Menu {
     [RequireComponent(typeof(AudioSource))]
     public class MainMenuMusic : MonoBehaviour {
         [SerializeField] private AudioClip musicIntro;
+        private float _volume;
+
+        private void Start() {
+            var audioSource = GetComponent<AudioSource>();
+            _volume = audioSource.volume;
+        }
 
         public void PlayMenuMusic(bool includeIntro) {
-            var source = GetComponent<AudioSource>();
+            var audioSource = GetComponent<AudioSource>();
             if (includeIntro) {
-                source.PlayOneShot(musicIntro);
-                source.PlayScheduled(AudioSettings.dspTime + musicIntro.length);
+                audioSource.PlayOneShot(musicIntro);
+                audioSource.PlayScheduled(AudioSettings.dspTime + musicIntro.length);
             }
             else {
-                source.Play();
+                audioSource.Play();
+            }
+        }
+
+        public void FadeOut(float duration = 1) {
+            var audioSource = GetComponent<AudioSource>();
+            audioSource.volume = _volume;
+            StartCoroutine(StartFade(audioSource, duration, 0));
+        }
+
+        public void FadeIn(float duration = 1) {
+            var audioSource = GetComponent<AudioSource>();
+            audioSource.volume = 0;
+            StartCoroutine(StartFade(audioSource, duration, _volume));
+        }
+
+        private static IEnumerator StartFade(AudioSource audioSource, float duration, float targetVolume) {
+            float currentTime = 0;
+            var start = audioSource.volume;
+            while (currentTime < duration) {
+                currentTime += Time.deltaTime;
+                audioSource.volume = Mathf.Lerp(start, targetVolume, currentTime / duration);
+                yield return null;
             }
         }
     }
