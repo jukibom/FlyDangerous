6b5a1f6e648c42b8e8451310311b44c2937fe663
Jay Faulkner <jayleefaulkner@gmail.com>
2022-05-05 19:02:40 +0100

    feat: music low pass filter on pause

--- Assets/Scripts/Core/AudioMixer.cs
+++ Assets/Scripts/Core/AudioMixer.cs
@@ -1,31 +1,36 @@
-using Core;
 using Misc;
 using UnityEngine;
 using UnityEngine.Audio;
 
-public class AudioMixer : Singleton<AudioMixer> {
-    [SerializeField] private AudioMixerGroup masterMixerGroup;
+namespace Core {
+    public class AudioMixer : Singleton<AudioMixer> {
+        [SerializeField] private AudioMixerGroup masterMixerGroup;
 
-    private void OnEnable() {
-        Game.OnGameSettingsApplied += OnSettingsApplied;
-        OnSettingsApplied();
-    }
+        private void OnEnable() {
+            Game.OnGameSettingsApplied += OnSettingsApplied;
+            OnSettingsApplied();
+        }
 
-    private void OnDisable() {
-        Game.OnGameSettingsApplied -= OnSettingsApplied;
-    }
+        private void OnDisable() {
+            Game.OnGameSettingsApplied -= OnSettingsApplied;
+        }
 
-    private void OnSettingsApplied() {
-        var master = MathfExtensions.Remap(0, 100, 0.0001f, 1, Preferences.Instance.GetFloat("volumeMaster"));
-        var music = MathfExtensions.Remap(0, 100, 0.0001f, 1, Preferences.Instance.GetFloat("volumeMusic"));
-        var sound = MathfExtensions.Remap(0, 100, 0.0001f, 1, Preferences.Instance.GetFloat("volumeSound"));
-        var ghostSound = MathfExtensions.Remap(0, 100, 0.0001f, 1, Preferences.Instance.GetFloat("volumeGhostSound"));
-        var ui = MathfExtensions.Remap(0, 100, 0.0001f, 1, Preferences.Instance.GetFloat("volumeUi"));
+        private void OnSettingsApplied() {
+            var master = MathfExtensions.Remap(0, 100, 0.0001f, 1, Preferences.Instance.GetFloat("volumeMaster"));
+            var music = MathfExtensions.Remap(0, 100, 0.0001f, 1, Preferences.Instance.GetFloat("volumeMusic"));
+            var sound = MathfExtensions.Remap(0, 100, 0.0001f, 1, Preferences.Instance.GetFloat("volumeSound"));
+            var ghostSound = MathfExtensions.Remap(0, 100, 0.0001f, 1, Preferences.Instance.GetFloat("volumeGhostSound"));
+            var ui = MathfExtensions.Remap(0, 100, 0.0001f, 1, Preferences.Instance.GetFloat("volumeUi"));
 
-        masterMixerGroup.audioMixer.SetFloat("masterVolume", Mathf.Log10(master) * 20);
-        masterMixerGroup.audioMixer.SetFloat("musicVolume", Mathf.Log10(music) * 20);
-        masterMixerGroup.audioMixer.SetFloat("soundVolume", Mathf.Log10(sound) * 20);
-        masterMixerGroup.audioMixer.SetFloat("ghostSoundVolume", Mathf.Log10(ghostSound) * 20);
-        masterMixerGroup.audioMixer.SetFloat("uiVolume", Mathf.Log10(ui) * 20);
+            masterMixerGroup.audioMixer.SetFloat("masterVolume", Mathf.Log10(master) * 20);
+            masterMixerGroup.audioMixer.SetFloat("musicVolume", Mathf.Log10(music) * 20);
+            masterMixerGroup.audioMixer.SetFloat("soundVolume", Mathf.Log10(sound) * 20);
+            masterMixerGroup.audioMixer.SetFloat("ghostSoundVolume", Mathf.Log10(ghostSound) * 20);
+            masterMixerGroup.audioMixer.SetFloat("uiVolume", Mathf.Log10(ui) * 20);
+        }
+
+        public void SetMusicLowPassEnabled(bool isEnabled) {
+            masterMixerGroup.audioMixer.SetFloat("musicCutoffFreq", isEnabled ? 480 : 22000);
+        }
     }
 }
--- Assets/Scripts/GameUI/PauseSystem.cs
+++ Assets/Scripts/GameUI/PauseSystem.cs
@@ -19,9 +19,11 @@ namespace GameUI {
         [UsedImplicitly]
         public void OnGameMenuToggle() {
             IsPaused = !IsPaused;
+            Game.Instance.PauseGameToggle(IsPaused);
+            AudioMixer.Instance.SetMusicLowPassEnabled(IsPaused);
+            menuContainer.SetActive(IsPaused);
+
             if (IsPaused) {
-                Game.Instance.PauseGameToggle(true);
-                menuContainer.SetActive(true);
                 user.DisableGameInput();
                 user.EnableUIInput();
                 user.ResetMouseToCentre();
@@ -29,8 +31,6 @@ namespace GameUI {
                 pauseMenu.PlayOpenSound();
             }
             else {
-                Game.Instance.PauseGameToggle(false);
-                menuContainer.SetActive(false);
                 user.EnableGameInput();
                 user.DisableUIInput();
                 user.ResetMouseToCentre();
@@ -50,36 +50,5 @@ namespace GameUI {
             Resume();
             Game.Instance.LeaveSession();
         }
-
-        // toggle ship controller input and timescales
-        // private void UpdatePauseGameState() {
-        //     Debug.Log(MenuState);
-        //     switch (MenuState) {
-        //         case PauseMenuState.PausedMainMenu:
-        //             Game.Instance.PauseGameToggle(true);
-        //             cursor.OnPointerMove(Vector2.zero);
-        //             _mainCanvas.enabled = true;
-        //             user.DisableGameInput();
-        //             user.EnableUIInput();
-        //             user.ResetMouseToCentre();
-        //             optionsPanel.Hide();
-        //             mainPanel.Show();
-        //             copyConfirmationText.color = new Color(1f, 1f, 1f, 0f);
-        //             break;
-        //         case PauseMenuState.Unpaused:
-        //             Game.Instance.PauseGameToggle(false);
-        //             _mainCanvas.enabled = false;
-        //             user.EnableGameInput();
-        //             user.DisableUIInput();
-        //             user.ResetMouseToCentre();
-        //             break;
-        //         case PauseMenuState.PausedOptionsMenu:
-        //             // TODO: use Progress instead (refactor this mad menu!!)
-        //             PlayOpenSound();
-        //             optionsPanel.Open(this);
-        //             mainPanel.Hide();
-        //             break;
-        //     }
-        // }
     }
 }
