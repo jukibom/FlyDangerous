a3aa1e96f537852284b452adf095d05d865796c0
Jay Faulkner <jayleefaulkner@gmail.com>
2022-05-05 19:01:42 +0100

    feat: more level music

--- Assets/Scripts/Audio/MusicTrack.cs
+++ Assets/Scripts/Audio/MusicTrack.cs
@@ -6,9 +6,21 @@ namespace Audio {
     public class MusicTrack : IFdEnum {
         private static int _id;
 
-        public static readonly MusicTrack MainMenu = new("Rock-Your-Body", "Main Menu/main menu loop", "Main Menu/main menu intro");
-        public static readonly MusicTrack Juno = new("Juno", "Levels/juno");
-        
+        public static readonly MusicTrack MainMenu = new("Rock-Your-Body", "Main Menu/rock_your_body_loop", "Main Menu/rock_your_body_intro");
+        public static readonly MusicTrack Juno = new("Juno", "Levels/juno_loop", "Levels/juno_intro");
+
+        public static readonly MusicTrack BeautifulCatastrophe =
+            new("Beautiful Catastrophe", "Levels/beautiful_catastrophe_loop", "Levels/beautiful_catastrophe_intro");
+
+        public static readonly MusicTrack DigitalBattleground =
+            new("Digital Battleground", "Levels/digital_battleground_loop", "Levels/digital_battleground_intro");
+
+        public static readonly MusicTrack ChaosAtTheSpaceship =
+            new("Chaos At The Spaceship", "Levels/chaos_at_the_spaceship_loop", "Levels/chaos_at_the_spaceship_intro");
+
+        public static readonly MusicTrack Hydra = new("Hydra", "Levels/hydra_loop", "Levels/hydra_intro");
+        public static readonly MusicTrack Hooligans = new("Hooligans", "Levels/hooligans_loop", "Levels/hooligans_intro");
+
         private MusicTrack(string name, string musicTrackToLoad, string introTrackToLoad = null) {
             Id = GenerateId;
             Name = name;
@@ -17,16 +29,21 @@ namespace Audio {
         }
 
         private static int GenerateId => _id++;
-        
-        public int Id { get; }
-        public string Name { get; }
         public string MusicTrackToLoad { get; }
         [CanBeNull] public string IntroTrackToLoad { get; }
         public bool HasIntro => IntroTrackToLoad != null;
-        
+
+        public int Id { get; }
+        public string Name { get; }
+
         public static IEnumerable<MusicTrack> List() {
             return new[] {
-                Juno
+                Juno,
+                BeautifulCatastrophe,
+                DigitalBattleground,
+                ChaosAtTheSpaceship,
+                Hydra,
+                Hooligans
             };
         }
 
