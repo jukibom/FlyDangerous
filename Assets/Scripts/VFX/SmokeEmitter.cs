ee43fd443a4ffd0d42469fdbbac7cfc3c2083baf
Jay Faulkner <jayleefaulkner@gmail.com>
2022-05-14 13:28:22 +0100

    feat: increase visibility of trails, scale + lifetime based on thrust
    this means a slow moving ship will generate less smoke and an idle ship will generate a very low amount of smoke

--- Assets/Scripts/VFX/SmokeEmitter.cs
+++ Assets/Scripts/VFX/SmokeEmitter.cs
@@ -12,6 +12,8 @@ namespace VFX {
         [SerializeField] private Vector3 maxEjectionSpeed = new(0, 0, -30);
         [SerializeField] private int minSpawnRate = 3;
         [SerializeField] private int maxSpawnRate = 150;
+        [SerializeField] private int minLifetime = 2;
+        [SerializeField] private int maxLifetime = 25;
 
         // fix problem with async updating with multiplayer initialisation (client can fail to update trails and disconnect with null exception)
         private bool _ready;
@@ -57,6 +59,19 @@ namespace VFX {
                     : MathfExtensions.Remap(0, maxSpeed, minSpawnRate, maxSpawnRate / 2f, vesselSpeedLocal.z);
 
                 _trailEffect.SetInt("_spawnRate", Mathf.FloorToInt(spawnRate));
+
+                // these get quite large quite quickly so lets cap their life dependent on speed
+                // we want them to be very visible at distance and at fast speeds and die quickly when stationary or slow
+                var lifetime = MathfExtensions.Remap(
+                    0,
+                    1,
+                    minLifetime,
+                    maxLifetime,
+                    force.z
+                );
+
+                _trailEffect.SetInt("_lifetimeMin", Mathf.FloorToInt(lifetime));
+                _trailEffect.SetInt("_lifetimeMax", Mathf.FloorToInt(lifetime + 5 * force.z));
 
                 // set jitter to a factor of the forward local velocity 
                 _trailEffect.SetVector3("_startingPositionJitter", new Vector3(0, 0, MathfExtensions.Remap(0, 1, 0, -5, vesselSpeedLocal.z / maxSpeed)));
