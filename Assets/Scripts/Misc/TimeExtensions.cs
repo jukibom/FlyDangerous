4248ec1575ac2218070ec6f3504302fe96161730
Jay Faulkner <jayleefaulkner@gmail.com>
2022-05-15 21:32:28 +0100

    fix: switch all timers over to millisecond accurate

--- Assets/Scripts/Misc/TimeExtensions.cs
+++ Assets/Scripts/Misc/TimeExtensions.cs
@@ -6,7 +6,7 @@ namespace Misc {
         public static string TimeSecondsToString(float timeSeconds) {
             var t = TimeSpan.FromSeconds(Mathf.Abs(timeSeconds));
             // return in 00:00:00 format (tens of milliseconds - we're only accurate to 1/50th of a second anyway!)
-            var time = $"{t.Minutes:D2}:{t.Seconds:D2}:{t.Milliseconds / 10:D2}";
+            var time = $"{t.Minutes:D2}:{t.Seconds:D2}:{t.Milliseconds:D3}";
             return timeSeconds < 0 ? $"-{time}" : time;
         }
     }
