fc4244f204ff6b668c2f2da231ee8863fb52a1ed
Jay Faulkner <jayleefaulkner@gmail.com>
2022-08-22 22:08:55 +0100

    feat: basic boost / collision bhaptics support!

--- Assets/Scripts/Core/ShipModel/Feedback/bHaptics/BHapticsShipFeedback.cs
+++ Assets/Scripts/Core/ShipModel/Feedback/bHaptics/BHapticsShipFeedback.cs
@@ -6,8 +6,22 @@ using UnityEngine;
 namespace Core.ShipModel.Feedback.bHaptics {
     public class BHapticsShipFeedback : MonoBehaviour, IShipFeedback, IShipInstruments {
         [SerializeField] private FeedbackEngine feedbackEngine;
-        [SerializeField] private HapticClip boostDropVestHapticSource;
-        [SerializeField] private HapticClip boostFireVestHapticSource;
+
+        [SerializeField] private VestHapticClip collisionImpactVestHapticClip;
+        [SerializeField] private VestHapticClip boostDropVestHapticClip;
+        [SerializeField] private VestHapticClip boostFireVestHapticClip;
+        [SerializeField] private VestHapticClip shipShakeVestHapticClip;
+        [SerializeField] private ArmsHapticClip collisionImpactLeftArmHapticClip;
+        [SerializeField] private ArmsHapticClip boostDropLeftArmHapticClip;
+        [SerializeField] private ArmsHapticClip boostFireLeftArmHapticClip;
+        [SerializeField] private ArmsHapticClip shipShakeLeftArmHapticClip;
+        [SerializeField] private ArmsHapticClip collisionImpactRightArmHapticClip;
+        [SerializeField] private ArmsHapticClip boostDropRightArmHapticClip;
+        [SerializeField] private ArmsHapticClip boostFireRightArmHapticClip;
+        [SerializeField] private ArmsHapticClip shipShakeRightArmHapticClip;
+
+        // No idea why but `IsPlaying()` always returns false :/
+        private float _shakeHapticPlayTime;
 
         private void OnEnable() {
             feedbackEngine.SubscribeFeedbackObject(this);
@@ -17,42 +31,34 @@ namespace Core.ShipModel.Feedback.bHaptics {
             feedbackEngine.RemoveFeedbackObject(this);
         }
 
-        // public void Collision(float forceNormalised, Vector3 direction) {
-        //     Debug.Log("Collision! " + forceNormalised + " " + direction);
-        //     
-        //     boostFireVestHapticSource.Play();
-        // }
-        //
-        // public void BoostDrop(float progressNormalised, bool firstFrame) {
-        //     Debug.Log("Boost Drop " + progressNormalised);
-        //     boostDropVestHapticSource.Play(1, 0.02f, 0, MathfExtensions.Remap(0, 1, 0.5f, -0.5f, progressNormalised));
-        // }
-        //
-        // public void Boost(float progressNormalised, bool firstFrame) {
-        //     Debug.Log("Boost " + progressNormalised);
-        //     // Fire once and rely on shake for the rest
-        //     if (firstFrame) {
-        //         boostFireVestHapticSource.Play();
-        //     }
-        // }
-        //
-        // public void ShipShake(float shakeAmountNormalised) {
-        //     Debug.Log("Ship Shake " + shakeAmountNormalised);
-        // }
-        //
-        // public void ShipIndicatorUpdate(IShipIndicatorData shipIndicatorData) {
-        //     
-        //     Debug.Log("ship indicators! yay!");
-        // }
-
         public void OnShipFeedbackUpdate(IShipFeedbackData shipFeedbackData) {
-            // if (shipFeedbackData.BoostDropStartThisFrame) Debug.Log("BOOST DROP START " + Time.frameCount);
-            // if (shipFeedbackData.BoostThrustStartThisFrame) Debug.Log("BOOST THRUST START " + Time.frameCount);
-            // if (shipFeedbackData.IsBoostDropActive) Debug.Log("Drop " + shipFeedbackData.BoostDropProgressNormalised);
-            // if (shipFeedbackData.IsBoostThrustActive) Debug.Log("Thrust " + shipFeedbackData.BoostThrustProgressNormalised);
-            //
-            // if (shipFeedbackData.IsBoostDropActive)
-            //     boostDropVestHapticSource.Play(0.5f, 1, 0, shipFeedbackData.BoostDropProgressNormalised);
+            if (shipFeedbackData.BoostDropStartThisFrame) {
+                boostDropVestHapticClip.Play(0.3f);
+                boostDropLeftArmHapticClip.Play(0.3f, 2);
+                boostDropRightArmHapticClip.Play(0.3f, 2);
+            }
+
+            if (shipFeedbackData.BoostThrustStartThisFrame) {
+                boostFireVestHapticClip.Play();
+                boostFireLeftArmHapticClip.Play();
+                boostFireRightArmHapticClip.Play();
+            }
+
+            if (shipFeedbackData.ShipShake > 0 && _shakeHapticPlayTime > 0.1f) {
+                _shakeHapticPlayTime = 0;
+                shipShakeVestHapticClip.Play(shipFeedbackData.ShipShake * 5, 0.1f);
+                shipShakeLeftArmHapticClip.Play(shipFeedbackData.ShipShake * 5, 0.2f);
+                shipShakeRightArmHapticClip.Play(shipFeedbackData.ShipShake * 5, 0.2f);
+            }
+
+            _shakeHapticPlayTime += Time.fixedDeltaTime;
+
+            if (shipFeedbackData.CollisionStartedThisFrame) {
+                collisionImpactVestHapticClip.Play(shipFeedbackData.CollisionImpactNormalised, 1, shipFeedbackData.CollisionDirection, Vector3.zero,
+                    Vector3.forward, 1);
+                collisionImpactLeftArmHapticClip.Play(shipFeedbackData.CollisionImpactNormalised);
+                collisionImpactRightArmHapticClip.Play(shipFeedbackData.CollisionImpactNormalised);
+            }
         }
 
         public void OnShipIndicatorUpdate(IShipInstrumentData shipInstrumentData) {
