274d89c831abe377572cbb21aacb7efc41b2105b
Jay Faulkner <jayleefaulkner@gmail.com>
2022-09-01 22:05:09 +0100

    fix: disable lens flares with MSAA and VR
    fucks sake guys

---
+++ Assets/Scripts/Misc/LensFlareEdgeCaseHandler.cs
@@ -0,0 +1,53 @@
+using System;
+using Core;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+namespace Misc {
+    [RequireComponent(typeof(LensFlareComponentSRP))]
+    public class LensFlareEdgeCaseHandler : MonoBehaviour {
+        private LensFlareComponentSRP _lensFlare;
+
+        private void Awake() {
+            _lensFlare = GetComponent<LensFlareComponentSRP>();
+        }
+
+        private void Start() {
+            OnApplySettings();
+        }
+
+        private void OnEnable() {
+            Game.OnRestart += OnApplySettings;
+            Game.OnGameSettingsApplied += OnApplySettings;
+            Game.OnVRStatus += OnVRStatus;
+        }
+
+        private void OnDisable() {
+            Game.OnRestart -= OnApplySettings;
+            Game.OnGameSettingsApplied -= OnApplySettings;
+            Game.OnVRStatus -= OnVRStatus;
+        }
+
+        private void OnApplySettings() {
+            HandleVisibility(Game.Instance.IsVREnabled);
+        }
+
+        private void OnVRStatus(bool IsEnabled) {
+            HandleVisibility(IsEnabled);
+        }
+
+        private void HandleVisibility(bool vrEnabled) {
+            var shouldShow = true;
+            
+            // TODO: remove this when #199 fixed (MSAA breaks lens flares!)
+            var urp = (UniversalRenderPipelineAsset)GraphicsSettings.currentRenderPipeline;
+            shouldShow &= urp.msaaSampleCount == 0;
+            
+            // TODO: remove this when #200 fixed (lens flare broken in VR!)
+            shouldShow &= !vrEnabled;
+
+            _lensFlare.enabled = shouldShow;
+        }
+    }
+}
