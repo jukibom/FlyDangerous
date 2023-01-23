1c7f2b34d85f9173ab793597902a3ed31a14a56c
Jay Faulkner <jayleefaulkner@gmail.com>
2023-01-23 23:31:03 +0000

    chore: reorder + rename locations

--- Assets/Scripts/Core/MapData/Location.cs
+++ Assets/Scripts/Core/MapData/Location.cs
@@ -7,17 +7,22 @@ namespace Core.MapData {
         private static int _id;
 
         // Declare locations here and add to the List() function below
+        public static readonly Location TerrainV3 = new("Mixed Terrain",
+            "A terrain with multiple mixed environments blended together over very large distances with water mechanics", "TerrainWorkspace", true);
+
+        public static readonly Location TestSpaceStation = new("Space Station", "An enormous test space station asset", "SpaceStation", false);
+
         public static readonly Location ProvingGrounds = new("Proving Grounds",
             "A testing scene used for staging new features and testing flight mechanics", "ProvingGrounds", false);
 
         public static readonly Location Space = new("Space", "Empty space - literally nothing here", "Space", false);
-        public static readonly Location TestSpaceStation = new("Space Station", "An enormous test space station asset", "SpaceStation", false);
-        public static readonly Location TerrainV1 = new("Flat World", "Terrain with peaks no higher than 2km", "TerrainV1", true);
-        public static readonly Location TerrainV2 = new("Canyons", "Terrain with peaks of 8km and deep, straight canyon grooves", "TerrainV2", true);
-        public static readonly Location TerrainV3 = new("Biome World", "A terrain with multiple mixed environments blended together", "TerrainV3", true);
 
-        public static readonly Location TerrainV4 = new("Biome World v2",
-            "A terrain with multiple mixed environments blended together over very large distances with water mechanics", "TerrainWorkspace", false);
+        public static readonly Location TerrainV1 = new("Mountains (Legacy)",
+            "Terrain with peaks no higher than 2km - only here for compatibility with legacy maps", "TerrainV1", true);
+
+        public static readonly Location TerrainV2 = new("Canyons (Legacy)",
+            "Terrain with peaks of 8km and deep, straight canyon grooves - only here for compatibility with legacy maps", "TerrainV2", true);
+
 
         private Location(string name, string description, string sceneToLoad, bool isTerrain) {
             Id = GenerateId;
@@ -36,7 +41,7 @@ namespace Core.MapData {
         public string Name { get; }
 
         public static IEnumerable<Location> List() {
-            return new[] { ProvingGrounds, Space, TestSpaceStation, TerrainV1, TerrainV2, TerrainV3, TerrainV4 };
+            return new[] { TerrainV3, TestSpaceStation, ProvingGrounds, Space, TerrainV1, TerrainV2 };
         }
 
         public static Location FromString(string locationString) {
