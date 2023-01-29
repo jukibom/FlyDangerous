423592f0ff0160ee5c53d597a36de828cc9de60f
Jay Faulkner <jayleefaulkner@gmail.com>
2023-01-29 15:36:27 +0000

    feat: make night vision more useful underwater

--- Assets/Scripts/Core/MapData/Environment.cs
+++ Assets/Scripts/Core/MapData/Environment.cs
@@ -12,7 +12,7 @@ namespace Core.MapData {
         public static readonly Environment SunsetClear = new("Sunset Clear", "Sunset_Clear", 3);
         public static readonly Environment SunsetCloudy = new("Sunset Cloudy", "Sunset_Cloudy", 3);
         public static readonly Environment NightClear = new("Night Clear", "Night_Clear", 4);
-        public static readonly Environment NightCloudy = new("Night Cloudy", "Night_Cloudy", 6);
+        public static readonly Environment NightCloudy = new("Night Cloudy", "Night_Cloudy", 5);
         public static readonly Environment PlanetOrbitBottom = new("Red Planet", "Planet_Orbit_Bottom", 2.5f);
         public static readonly Environment PlanetOrbitTop = new("Blue Planet", "Planet_Orbit_Top", 1.8f);
         public static readonly Environment RedBlueNebula = new("Red / Blue Nebula", "Red_Blue_Nebula", 2.8f);
